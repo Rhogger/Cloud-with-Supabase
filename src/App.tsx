c2652f4a52e74f1a57a902e725bdbefe01ad40e5
rhogger <rhoggerrv@gmail.com>
2023-11-12 00:25:51 -0300

    :construction: feat: UI desenvolvida para teste, ou seja, nao está finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import PlusIcon from './assets/icons/plus-circle.svg';
+
+import { createClient } from '@supabase/supabase-js';
+import { Auth } from '@supabase/auth-ui-react';
+import {
+  // Import predefined theme
+  ThemeSupa,
+} from '@supabase/auth-ui-shared';
+
+const supabase = createClient(process.env.);
+
+// const App = () => <Auth
+// supabaseClient={supabase}
+// appearance={{ theme: ThemeSupa }}
+// />
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className='flex flex-col justify-center items-center gap-7 h-screen'>
+      <h1 className='text-4xl font-bold'>Save ur images</h1>
+
+      <label
+        htmlFor='upload'
+        className='flex flex-col justify-center items-center gap-4 w-96 h-48 border border-dashed rounded-2xl p-6'>
+        <img src={PlusIcon} alt='Arrow Icon' className='w-14' />
+
+        <h2 className='text-lg font-bold text-zinc-300'>Drag here</h2>
+      </label>
+
+      <input
+        className='w-96 file:font-medium file:bg-purple-700 hover:file:bg-purple-600 focus:file:bg-purple-600 file:border file:border-white file:rounded-md file:p-2 file:mr-4 focus:outline-0'
+        type='file'
+        name='upload'
+        id='upload'
+      />
+
+      <button className='text-xl font-semibold py-3 px-8 border-2 border-purple-700 hover:border-purple-500 focus:border-purple-500 focus:outline-0 rounded-2xl mt-4'>
+        Send
+      </button>
+    </main>
+  );
 }
 
-export default App
+export default App;
