179856240d5fb8dd099d19056c909c305240d5c8
rhogger <rhoggerrv@gmail.com>
2023-11-15 21:26:57 -0300

    :heavy_minus_sign: build: Removendo dependencias e ajustanddo configs

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Upload from '../pages/Upload';
 import Authentication from '../pages/Authentication';
 import Gallery from '../pages/Gallery';
-import ProtectedRoute from '../components/ProtectedRoute';
+// import ProtectedRoute from '../components/ProtectedRoute';
 import Image from '../pages/Image';
 
 const router = createBrowserRouter([
